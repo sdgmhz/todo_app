02c2cd5bcb97824582fa9e51cd5e64bacb5f5e13
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2024-11-08 20:53:53 +0330

    create accounts app & create CustomUser model

---
+++ accounts/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        '''
+        create and save a user with the given email and password and extra data.
+        '''
+        if not email:
+            raise ValueError(_("The email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        '''
+        create and save a SuperUser with the given email and password
+        '''
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True"))
+        
+        return self.create_user(email, password, **extra_fields)
+    
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    ''''
+    class for custom user model using email address
+    '''
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    objects = CustomUserManager()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+
