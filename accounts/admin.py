400453a6d2a3b63053a547a9fb6ee09257bb94fb
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2024-11-08 21:20:17 +0330

    add CustomUser to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+user_model = get_user_model()
+
+@admin.register(user_model)
+class CustomUserAdmin(UserAdmin):
+    model = user_model
+    list_display = ('email', 'is_superuser', 'is_active',)
+    list_filter = ('email', 'is_superuser', 'is_active',)
+    search_fields = ('email', )
+    ordering = ('email', )
+    fieldsets =(
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser',)}),
+        ('group permissions', {'fields': ('groups', 'user_permissions',)}),
+        ('login', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'),
+        }),
+    )
