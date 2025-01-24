8d6fb85ee1c8d97497f8e23dc8fb9910873f81b8
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2025-01-24 19:05:22 +0330

    initiate api authentication process

---
+++ accounts/api/v1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+urlpatterns = [
+    # registration
+    # change password
+    # reset password
+    # login token
+    # login jwt
+]
+
--- config/urls.py
+++ config/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
     path('duties/', include('duties.urls')),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    
     path('403/', TemplateView.as_view(template_name="403.html")),
     path('api-auth/', include('rest_framework.urls')),
 
